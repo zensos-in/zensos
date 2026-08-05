4ea4a9e8ffe715784364dd88fc79476bb95150e9
Kishore V <kishore.v@exathought.com>
2026-08-05 13:11:55 +0530

    error fix

--- server/src/routes/uploadRoutes.js
+++ server/src/routes/uploadRoutes.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const auth = require("../middleware/auth");
-const adminAuth = require("../middleware/adminAuth");
 const {
   generateUploadPresignedUrl,
   generateViewPresignedUrl,
